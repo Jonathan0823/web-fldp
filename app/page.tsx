933f1ecf0c8275b96c42f6a8e7bd68456447f787
Guinn <eguinjonathan08@gmail.com>
2024-10-18 12:37:40 +0700

    Refactor HomeContent component to add dynamic content based on active button

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default async function Home() {
   const session = await getServerSession();
 
   return (
-    <div className="min-h-dvh flex items-center justify-center bg-[#eff1f3]">
-      <div className="bg-[#f3f4f6] p-8 rounded-lg md:max-w-md max-w-sm w-full shadow-md min-h-dvh">
+    <div className="min-h-dvh flex items-center justify-center bg-[#e7eaed]">
+      <div className="bg-[#eff1f3] p-8 rounded-lg md:max-w-md max-w-sm w-full shadow-md min-h-dvh">
         <div className="flex justify-between">
           <h2 className="font-bold text-2xl w-3/5">
             Selamat datang, {session?.user?.name}
