865056d723a1d20829f1c1c9fe2c4a580284bba3
Guinn <eguinjonathan08@gmail.com>
2024-11-04 12:53:42 +0700

    Refactor ListDosen component to update profile display and improve data fetching

--- app/components/ListDosen.tsx
+++ app/components/ListDosen.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { StarIcon } from "@heroicons/react/20/solid";
 import axios from "axios";
-
+import { IoMdStarOutline } from "react-icons/io";
 interface DosenProfile {
   nama: string;
-  mataKuliah: string;
+  matakuliah: string;
   rating: string; 
   reviews: number;
 }
@@ -19,6 +18,7 @@ export default function ProfileList() {
       try {
         const result = await axios.get(`/api/getDosen/All`);
         setDosen(result.data);
+        console.log("Data fetched:", result.data);
       } catch (e) {
         console.error("Error fetching data:", e);
       }
@@ -36,10 +36,10 @@ export default function ProfileList() {
               className="p-4 bg-white rounded-lg shadow flex items-center justify-between"
             >
               <div>
-                <h3 className="text-lg font-medium text-gray-900">{profile.nama}</h3>
-                <p className="text-sm text-gray-600">{profile.mataKuliah}</p>
-                <div className="flex items-center mt-1">
-                  <StarIcon className="w-5 h-5 text-yellow-400" />
+                <h3 className="text-lg font-semibold text-gray-900">{profile.nama}</h3>
+                <p className="text-sm text-gray-600">Spesialisasi: {profile.matakuliah}</p>
+                <div className="flex items-center mt-3">
+                  <IoMdStarOutline className="w-7 h-7 text-yellow-400" />
                   <span className="ml-1 text-sm text-gray-600">
                     {profile.rating}/5 ({profile.reviews} penilaian)
                   </span>
