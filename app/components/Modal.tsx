3cb5ea9c64005847c11e457d76484579c3122ede
Guinn <eguinjonathan08@gmail.com>
2024-11-07 06:20:01 +0700

    Refactor Modal component for improved readability and maintainability; add TODO comments for future enhancements

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,45 +1,41 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 import { IoMdClose } from "react-icons/io";
 
 interface ModalProps {
-    type: "detail" | "rate";
-    userId: string;
+  type: "detail" | "rate";
+  userId: string;
 }
 
-const Modal = ({type, userId}: ModalProps) => {
+const Modal = ({ type, userId }: ModalProps) => {
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
       <div className="p-8 relative border w-full shadow-lg rounded-md md:max-w-md max-w-sm h-dvh bg-white">
         <div className="text-center">
-          <h3 className="text-2xl font-bold text-gray-900">{
-            type === "detail" ? "Detail Dosen" : "Beri Nilai"
-            }</h3>
+          <h3 className="text-2xl font-bold text-gray-900">
+            {type === "detail" ? "Detail Dosen" : "Beri Nilai"}
+          </h3>
           <div className="mt-2 px-7 py-3">
             {type === "detail" ? (
-                <div>
-                    Detail Dosen
-                </div>
+              //   Todo: Add detail dosen
+              <div>Detail Dosen</div>
             ) : (
-                <div>
-                    Beri Nilai
-                </div>
+                // Todo: Add rate dosen
+              <div>Beri Nilai</div>
             )}
           </div>
           <div className="flex justify-center mt-4">
-
             <Link
               href="/"
-              className='absolute top-9 right-7 text-gray-400 hover:text-red-500'
+              className="absolute top-9 right-7 text-gray-400 hover:text-red-500"
             >
-                <IoMdClose className="w-6 h-6" />
+              <IoMdClose className="w-6 h-6" />
             </Link>
-
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
