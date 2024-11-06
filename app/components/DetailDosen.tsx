5512a17d153d664ccfb869cfcf970d81d357a3f2
Guinn <eguinjonathan08@gmail.com>
2024-11-07 06:49:11 +0700

    Enhance DetailDosen component with TypeScript types and detailed rendering of dosen information

--- app/components/DetailDosen.tsx
+++ app/components/DetailDosen.tsx
@@ -6,7 +6,17 @@ import React, { useEffect } from "react";
 const DetailDosen = () => {
   const params = useSearchParams();
   const dosenId = params.get("dosenId");
-  const [dosen, setDosen] = React.useState(null);
+  interface Dosen {
+    nama: string;
+    nip: string;
+    email: string;
+    prodi: string;
+    fakultas: string;
+    matakuliah: string;
+    rating: number;
+  }
+
+  const [dosen, setDosen] = React.useState<Dosen | null>(null);
 
   useEffect(() => {
     const fetchDosen = async () => {
@@ -23,7 +33,24 @@ const DetailDosen = () => {
     }
   }, [dosenId]);
 
-  return <div>{dosenId}</div>;
+  return (
+    <div>
+      {dosen && (
+        <div className="flex flex-col">
+          <div className="text-left">
+            <h1>{dosen.nama}</h1>
+            <p>{dosen.nip}</p>
+            <p>{dosen.email}</p>
+            <p>{dosen.prodi}</p>
+            <p>{dosen.fakultas}</p>
+            <p>{dosen.matakuliah}</p>
+            <p>{dosen.rating}</p>
+          </div>
+          <h2>Penilaian</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default DetailDosen;
