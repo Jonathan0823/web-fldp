0da45d05ecb0fd83871fdb73aea7266d536c067b
Guinn <eguinjonathan08@gmail.com>
2024-11-09 06:47:26 +0700

    Refactor LogOut component for improved formatting and add Admin link for user role

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { authOptions } from "@/lib/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-  
 
   return (
     <div className="min-h-dvh flex items-center justify-center bg-[#e7eaed]">
@@ -14,9 +13,19 @@ export default async function Home() {
           <h2 className="font-bold text-2xl w-3/5">
             Selamat datang, {session?.user?.name}
           </h2>
-          <LogOut />
+          {session?.user?.role === "Admin" && (
+            <a
+              href="/admin-dashboard"
+              className="bg-black text-white p-2 h-10 rounded-lg"
+            >
+              Admin
+            </a>
+          )}
+          <div>
+            <LogOut />
+          </div>
         </div>
-        <HomeContent session={session?.user?.id || ""}  />
+        <HomeContent session={session?.user?.id || ""} />
       </div>
     </div>
   );
